bd4663ff294e179cdaed85d1082255d8507e065f
Ben <benawhiting@gmail.com>
2023-03-13 15:44:27 -0700

    Link form to backend

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Form: React.FC = () => {
   const [msg, setMsg] = useState('');
@@ -7,8 +8,14 @@ const Form: React.FC = () => {
     setMsg(e.target.value);
   }
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const body = { message: msg };
+    axios.post('http://localhost:3001', body);
+  }
+
   return (
-    <form action="">
+    <form action="" onSubmit={(e) => handleSubmit(e)}>
       <input type="text" onChange={(e) => updateMsg(e)} />
       <button>Submit</button>
     </form>
