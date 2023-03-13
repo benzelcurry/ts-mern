929502756dea558c2d7e6bedfa244dc1683ea12a
Ben <benawhiting@gmail.com>
2023-03-13 15:25:43 -0700

    Map response objects to list elements

--- client/src/components/Display.tsx
+++ client/src/components/Display.tsx
@@ -1,9 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import { Message } from '../message.model';
 
 const Display: React.FC = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    axios.get<Message>('http://localhost:3001')
+      .then((response) => {
+        setMessages([...messages, response.data])
+      });
+  }, []);
+
   return (
     <ul>
-
+      {
+        messages.map(message =>
+          <li key={Math.random()}>{message.message}</li>
+        )
+      }
     </ul>
   );
 };
