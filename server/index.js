4454f66b058f8135bdfb6629470e23b0cb4aae3e
Ben <benawhiting@gmail.com>
2023-03-13 15:50:37 -0700

    Update server to allow for new messages being pushed from frontend

--- server/index.js
+++ server/index.js
@@ -4,11 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors = require('cors');
 const app = (0, express_1.default)();
+app.use(cors());
 const port = 3001;
+const messages = [];
 app.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Hello, world!'
-    });
+    res.status(200).json(messages);
+});
+app.post('/', (req, res, next) => {
+    const newMsg = { message: req.body.message };
+    messages.push(newMsg);
 });
 app.listen(port, () => console.log(`Running on Port ${port}`));
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response, NextFunction } from 'express';
+const cors = require('cors');
 const app = express();
+app.use(cors());
 const port = 3001;
 
+const messages: {message: string}[] = [];
+
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json({
-    message: 'Hello, world!'
-  });
+  res.status(200).json(messages);
 });
+
+app.post('/', (req: Request, res: Response, next: NextFunction) => {
+  const newMsg: {message: string} = {message: req.body.message};
+  messages.push(newMsg);
+})
 
 app.listen(port, () => console.log(`Running on Port ${port}`));
