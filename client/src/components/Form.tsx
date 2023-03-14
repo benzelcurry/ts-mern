5ff5d6a7bd5101095150aaea7e2aae0642053c94
Ben <benawhiting@gmail.com>
2023-03-13 20:28:42 -0700

    Send additional info to server

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -10,22 +10,39 @@ import axios from 'axios';
 
 const Form: React.FC = () => {
   const [msg, setMsg] = useState('');
+  const [author, setAuthor] = useState('');
+  const [numbers, setNumbers] = useState<number>();
 
   const updateMsg = (e: React.ChangeEvent<HTMLInputElement>) => {
     setMsg(e.target.value);
   }
 
+  const updateAuthor = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAuthor(e.target.value);
+  }
+
+  const updateNumbers = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNumbers(Number(e.target.value));
+  }
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    axios.post('http://localhost:3001/', { msg: msg })
-      .then((response) => {
-        console.log(response);
+    axios.post('http://localhost:3001/', 
+      { 
+        msg: msg,
+        author: author,
+        numbers: numbers
       })
+        .then((response) => {
+          console.log(response);
+        })
   }
 
   return (
     <form action="" onSubmit={(e) => handleSubmit(e)}>
       <input type="text" onChange={(e) => updateMsg(e)} />
+      <input type="text" onChange={(e) => updateAuthor(e)}/>
+      <input type="number" onChange={(e) => updateNumbers(e)} />
       <button>Submit</button>
     </form>
   );
